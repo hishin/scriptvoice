139b142b8d9073080e657946bd693b063e1b7548
Hijung Shin <hishin@mit.edu>
2017-05-04 11:36:23 -0400

    update

--- initpage.js
+++ initpage.js
@@ -13,6 +13,32 @@ var trackchange = false;
 var aligned = true;
 var printall_selected = null;
 
+$(document).ready(function() {
+    //toggle `popup` / `inline` mode
+    $.fn.editable.defaults.mode = 'popup';     
+    
+    //make username editable
+    $('#username').editable();
+    
+    //make status editable
+    $('#status').editable({
+        type: 'select',
+        title: 'Select status',
+        placement: 'right',
+        value: 2,
+        source: [
+            {value: 1, text: 'status 1'},
+            {value: 2, text: 'status 2'},
+            {value: 3, text: 'status 3'}
+        ]
+        /*
+        //uncomment these lines to send data on server
+        ,pk: 1
+        ,url: '/post'
+        */
+    });
+});
+
 $(window).load(
 		function() {
 
@@ -38,7 +64,7 @@ $(window).load(
 						reprint();
 					});
 
-			loadMasterscript('walker_masterscript.json');
+			loadMasterscript('outline.json');
 			// masterscript = new Script();
 			nw = new NeedlemanWunsch();
 
